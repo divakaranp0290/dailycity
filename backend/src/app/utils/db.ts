51d67893749d9768378310c5036aee6041c10221
Divakaran <divakaranp0290@gmail.com>
2026-01-06 13:02:35 +0530

    API Commit

--- backend/src/app/utils/db.ts
+++ backend/src/app/utils/db.ts
@@ -3,10 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Local setup
+// export const db = new Pool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+
+// Production setup for Render
 export const db = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
